8c4cac9d3df9919631156b9bdd0e8a9d6491843a
hackdanismo <office@justtoconfirm.co.uk>
2026-06-14 13:46:52 +0100

    feat: add the initial navigation component to the application with links between the news and home pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Navigation from "@/components/navigation"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navigation />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,6 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
     <>
-      <nav>
-        <Link href="/news">News</Link>
-      </nav>
-
       <h1>Home</h1>
     </>
   );
