92f70ddaffa458f264bbf0f3f1132768dae280d1
hackdanismo <office@justtoconfirm.co.uk>
2026-06-14 13:40:50 +0100

    feat: add link to the news page from the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
+      <nav>
+        <Link href="/news">News</Link>
+      </nav>
+
       <h1>Home</h1>
     </>
   );
